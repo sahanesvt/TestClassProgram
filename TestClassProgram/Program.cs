c897dc6e1daa40718dc3e4fa7a3401aa32c8ae29
sahanes <sahanes_2000@yahoo.com>
2017-05-10 22:20:11 -0400

    Add project files.

---
+++ TestClassProgram/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestClassProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
